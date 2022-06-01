31b78340930ba6223f41ec69572fa327eb93f259
Bhupinder Singh <bhupsdabb@gmail.com>
2022-06-01 10:57:33 -0400

    added project files for week3

---
+++ Week3/.history/server_20220516091013.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function(req, res)
+{
+    console.log(__dirname);
+
+    //fs.readFile(__dirname + req.url)
+}); 
+
+server.listen(port, function)
---
+++ Week3/.history/server_20220516094035.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 3000;
+
+const server = http.createServer(function(req, res)  
+{
+    fs.readFile(__dirname + req.url, function(err, data)
+    {
+        if(err)
+        {
+            res.writeHead(404); // status - file not found
+            console.log(`ERROR ${err.message}`); //output the error message to the page
+            res.end(err.message);
+            return res.end(err.message);
+        }
+        res.writeHead(200);
+        res.end(data);
+        console.log(`DATA: ${data}`);
+    });
+}); 
+
+server.listen(port, function()
+{
+    console.log(`Server running at Port: ${port}`);
+});
